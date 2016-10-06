3057c0d93f419181baf303de2e5a4696699fa3c3
Andy <Andy PC>
2016-10-06 01:12:17 -0700

    comments

--- reader.py
+++ reader.py
@@ -1,1 +1,30 @@
 __author__ = 'Andy'
+
+import csv
+"""
+Categories as follows:
+
+
+Income
+Bookkeeping
+Rent - retail
+Rent - storage
+Cash Wage
+Food Cost
+Tax
+Payroll
+Lawyer James
+POS Fees
+Meal & Entertainment
+Office Supplies
+Refund
+Transfer to Savings
+One Time
+Ice Cream
+Transfer from Savings
+Credit Card Payment
+Bank Fee
+"""
+
+def readcsv(filename):
+	reader = csv.DictReader(open(filename))
